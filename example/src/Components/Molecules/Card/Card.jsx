b8941695afb2f8cdf5df306b3643b6d801605d70
Patrick Burris <patrick@briefmedia.com>
2017-02-11 01:02:09 -0600

    create card

--- example/src/Components/Molecules/Card/Card.jsx
+++ example/src/Components/Molecules/Card/Card.jsx
@@ -1,21 +1,28 @@
 import React, { PropTypes } from 'react';
 
 
-const Profile = (props) => (
-  <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
-    <div className="tc">
-      <h1 className="f3 mb2">{ props.name }</h1>
-      <h2 className="f5 fw4 gray mt0">{ props.login }</h2>
+const Card = (props) => (
+  <section className="tc pa3 pa5-ns">
+    <article className="hide-child relative ba b--black-20 mw5 center">
       <img
         src={ props.avatar_url }
-        className="br-100 h4 w4 dib ba b--black-05 pa2"
-        title={ props.login } />
-    </div>
-  </article>
+        className="db"
+        alt={ props.name} />
+      <div className="pa2 bt b--black-20">
+        <a className="f6 db link dark-blue hover-blue" href={ `https://www.github.com/${props.name}`}>{ props.name }</a>
+        <p className="f6 gray mv1">{ props.login }</p>
+        <p className="f6 gray mv1">Public Repos: { props.public_repos }</p>
+      </div>
+      <a className="child absolute top-1 right-1 ba bw1 black-40 grow no-underline br-100 w1 h1 pa2 lh-solid b" href="#">×</a>
+    </article>
+  </section>
 );
 
-Profile.propTypes = {
+
+Card.propTypes = {
   name: PropTypes.string.isRequired,
   login: PropTypes.string.isRequired,
   avatar_url: PropTypes.string.isRequired,
 };
+
+export default Card;
